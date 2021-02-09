47c93fa55b66768f313c08a724b3febdb925f8e1
VladimirKotevski <kotebt.vk@gmail.com>
2021-02-09 09:44:14 +0100

    design update

--- src/components/GamesPage.js
+++ src/components/GamesPage.js
@@ -5,32 +5,37 @@ import { logout } from '../actions/users';
 
 class GamesPage extends React.Component {
   render() {
-    console.log(this.props)
     this.state = {
-        games: [{title: 'First game', description: 'First game description'},
-        {title: 'Second game', description: 'Second game description'},
-        {title: 'Third game', description: 'Third game description'},
-        {title: 'Forth game', description: 'Forth game description'}
+        games: [{title: 'Book of Dead', image: 'book-of-dead', popular: false},
+        {title: 'Solar Queen', image: 'andar-bahar', popular: true},
+        {title: 'Razor Shark', image: 'blackjack', popular: false},
+        {title: 'Starburst', image: 'teen-patti', popular: false},
+        {title: 'Fire Joker', image: 'casino-scene', popular: false},
+        {title: 'Crazy Time', image: 'book-of-dead', popular: false},
+        {title: 'Andar Bahar', image: 'andar-bahar', popular: false},
+        {title: 'Roulette', image: 'casino-scene', popular: true},
+        {title: 'Blackjack', image: 'blackjack', popular: false},
+        {title: 'Teen Patti', image: 'teen-patti', popular: false},
         ],
         user: this.props.user[0] 
     };
     return (
       <React.Fragment>
-        <Container>
+        <Container className="games-container">
           <Row className="text-center">
             {this.state.games.map((game, index) => {
                 return ([
-                <Col className="game-box" key={index} md={4} xs={6}>
-                <p>{game.title}</p>
-                <p>{game.description}</p>
-                <Button variant="primary" size="lg">
-                {this.state.user ? (
-                  <div>
-                Play for real
-                </div>
-                ) : <div>Play for fun</div>
-              }
-                </Button>
+                <Col className={game.popular == true ? 'game-box col-md-6 col-12' : 'game-box col-md-3 col-12'} key={index} >
+                 <img className="game-image" src={window.location.origin + '/images/' + game.image + '.jpg'} alt=""></img>
+                 <Button className="play-button" variant="primary" size="lg">
+                  {this.state.user ? (
+                  <div className="play-text">
+                    Play for real
+                  </div>
+                  ) : <div className="play-text">Play for fun</div>
+                  }
+                  </Button>
+                    <p className="game-title">{game.title}</p>
                 </Col>
                 ]);
             })}    
